5bfec3b8e32788fde1519020abd4624f2fc7020f
HARISHCHANDRA214 <harishchandramardi123@gmail.com>
2023-11-20 09:56:05 +0530

    Modification in reactrouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
+import Booking from './components/Booking';
+import LoginPage from './components/LoginPage';
+import ConfirmationPage from './components/ConfirmationPage';
+import ProfilePage from './components/ProfilePage';
+import { BrowserRouter, Routes, Route ,Navigate} from "react-router-dom";
 
 function App() {
   return (
-    <HomePage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/booking" element={<Booking/>}/>
+        <Route path="/confirmationpage" element={<ConfirmationPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/profilepage" element={<ProfilePage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
